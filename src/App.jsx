891da61bf7949c4e9d8aeda6622905a693116af4
MuhammadMuneebSiddiqui <siddiquimuneeb894@gmail.com>
2025-09-22 12:53:56 +0500

    Website Clone

--- src/App.jsx
+++ src/App.jsx
@@ -14,31 +14,6 @@ gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
 
-    useEffect(() => {
-    const lenis = new Lenis({
-      duration: 2,
-      smooth: true,
-      smoothTouch: false,
-      direction: "vertical",
-      gestureDirection: "vertical",
-    });
-
-    lenis.on("scroll", ScrollTrigger.update);
-
-    gsap.ticker.add((time) => {
-      lenis.raf(time * 1000)
-    })
-
-    gsap.ticker.lagSmoothing(0)
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  }, []);
-
   return (
     <section className='text-4xl'>
       <NavBar />
